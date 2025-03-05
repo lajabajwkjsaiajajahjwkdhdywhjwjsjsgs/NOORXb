aa2cf0f0281beeaf286809e0c6e9eb9d2f5cf243
lajabajwkjsaiajajahjwkdhdywhjwjsjsgs <terabaaphume120@gmail.com>
2025-03-05 11:29:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,12 +11,12 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7520092354"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/auntyxd12/XXCHATBOTXX")
+OWNER_ID = int(getenv("OWNER_ID", "7995626429"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/lajabajwkjsaiajajahjwkdhdywhjwjsjsgs/NOORXb")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "ZOYU_SUPPORT"
-UPDATE_CHNL = "THE_INCRICIBLE"
-OWNER_USERNAME = "LEGEND_MICKEY"
+SUPPORT_GRP = "THUNDER_SUPPORT_ll"
+UPDATE_CHNL = "THUNDERDEVS"
+OWNER_USERNAME = "ll_ISTKHAR_BABY_lll"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
     
